d1e6a81b0e167aaa4688b965e0ac6e23c69a61e2
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-12-07 13:18:28 +0000

    [Add] 1-7 データベース上に必要なテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_07_114410) do
+ActiveRecord::Schema.define(version: 2023_12_07_125935) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
