15ecffd843503bc790bf03460e84a5c4b7cda3ec
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-12-07 12:53:12 +0000

    [Add] 1-5 deviseを使用するviewの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行され
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #protectedは呼び出された他のコントローラからも参照することができる。
+  protected
+
+  # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことでユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可。ストロングパラメータと同等の役割
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
