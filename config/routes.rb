3ad7f5b4cdc298f92c3f8f2d475ca25449e02461
pyusuket <101777200+pyusuket@users.noreply.github.com>
2023-12-09 06:26:37 +0000

    [Add] 1-11 home/about/usersのビューファイルを作成とコントローラとの紐づけ

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   resource :homes
-  
+  resource :users
+  get 'about' => 'homes#about'
+  get 'top' => 'homes#top'
 end
